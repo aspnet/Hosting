0d5aee0b04c7338afc200198360f27fb59aadaee
Master T <tom.deseyn@gmail.com>
2015-08-26 23:06:12 +0200

    Code cleanup: TestWebSocket: add copyright and remove unnecessary usings

--- src/Microsoft.AspNet.TestHost/TestWebSocket.cs
+++ src/Microsoft.AspNet.TestHost/TestWebSocket.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.WebSockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
