fc4c3316d97421904c0557071c134527bb36c263
Master T <tom.deseyn@gmail.com>
2015-09-02 17:50:24 +0200

    Add ClientCancellationAbortsRequest test, use ThrowsAnyAsync as a derived class of OperationCanceledException may also be thrown (TaskCanceledException)

--- test/Microsoft.AspNet.TestHost.Tests/TestClientTests.cs
+++ test/Microsoft.AspNet.TestHost.Tests/TestClientTests.cs
@@ -144,7 +144,36 @@ namespace Microsoft.AspNet.TestHost
             response.Dispose();
 
             // Assert
-            var exception = await Assert.ThrowsAsync<OperationCanceledException>(async () => await tcs.Task);
+            var exception = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () => await tcs.Task);
+        }
+
+        [Fact]
+        public async Task ClientCancellationAbortsRequest()
+        {
+            // Arrange
+            TaskCompletionSource<object> tcs = new TaskCompletionSource<object>();
+            RequestDelegate appDelegate = async ctx =>
+            {
+                var sem = new SemaphoreSlim(0);
+                try
+                {
+                    await sem.WaitAsync(ctx.RequestAborted);
+                }
+                catch (Exception e)
+                {
+                    tcs.SetException(e);
+                }
+            };
+
+            // Act
+            var server = TestServer.Create(app => app.Run(appDelegate));
+            var client = server.CreateClient();
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+            var response = await client.GetAsync("http://localhost:12345", cts.Token);
+
+            // Assert
+            var exception = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () => await tcs.Task);
         }
     }
 }
