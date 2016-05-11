5a4747536738f8ecf892479ea829b84c9f74073c
Kiran Challa <kichalla@microsoft.com>
2016-05-11 13:23:09 -0700

    updated

--- src/Microsoft.AspNetCore.Server.Testing/Deployers/RemoteWindowsDeployer/RemoteWindowsDeployer.cs
+++ src/Microsoft.AspNetCore.Server.Testing/Deployers/RemoteWindowsDeployer/RemoteWindowsDeployer.cs
@@ -59,12 +59,6 @@ namespace Microsoft.AspNetCore.Server.Testing
                     " . A file share is required to copy the application's published output.");
             }
 
-            if (string.IsNullOrWhiteSpace(_deploymentParameters.RemoteServerRelativeExecutablePath))
-            {
-                throw new ArgumentException($"Invalid value for {nameof(RemoteWindowsDeploymentParameters.RemoteServerRelativeExecutablePath)}." +
-                    " This is the name of the executable in the published output which needs to be executed on the remote server.");
-            }
-
             if (string.IsNullOrWhiteSpace(_deploymentParameters.ApplicationBaseUriHint))
             {
                 throw new ArgumentException($"Invalid value for {nameof(RemoteWindowsDeploymentParameters.ApplicationBaseUriHint)}.");
