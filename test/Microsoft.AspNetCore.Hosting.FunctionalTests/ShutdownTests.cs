4e7de6ab458b0b08d5d4d6cdd95daaf592871f78
Pavel Krymets <pavel@krymets.com>
2016-11-02 13:55:42 -0700

    Add test for SIGTERM functionality

---
+++ test/Microsoft.AspNetCore.Hosting.FunctionalTests/ShutdownTests.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.IntegrationTesting;
+using Microsoft.AspNetCore.Server.IntegrationTesting.xunit;
+using Microsoft.AspNetCore.Testing.xunit;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Microsoft.AspNetCore.Hosting.FunctionalTests
+{
+    public class ShutdownTests
+    {
+        [ConditionalFact]
+        [OSSkipCondition(OperatingSystems.Windows)]
+        public async Task ShutdownTest()
+        {
+            var logger = new LoggerFactory()
+                .AddConsole()
+                .CreateLogger(nameof(ShutdownTest));
+
+            string applicationPath = Path.Combine(TestProjectHelpers.GetProjectRoot(), "..",
+                "Microsoft.AspNetCore.Hosting.TestSites");
+
+            var deploymentParameters = new DeploymentParameters(
+                applicationPath,
+                ServerType.Kestrel,
+                RuntimeFlavor.CoreClr,
+                RuntimeArchitecture.x64)
+            {
+                EnvironmentName = "Shutdown",
+                TargetFramework = "netcoreapp1.1",
+                ApplicationType = ApplicationType.Portable,
+                PublishApplicationBeforeDeployment = true
+            };
+
+            using (var deployer = new SelfHostDeployer(deploymentParameters, logger))
+            {
+                var deploymentResult = deployer.Deploy();
+                var httpClientHandler = new HttpClientHandler();
+                var httpClient = new HttpClient(httpClientHandler)
+                {
+                    BaseAddress = new Uri(deploymentResult.ApplicationBaseUri)
+                };
+
+                var response = await RetryHelper.RetryRequest(
+                    () => httpClient.GetAsync(string.Empty),
+                    logger,
+                    deploymentResult.HostShutdownToken);
+
+                var responseText = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    Assert.Equal("Hello World", responseText);
+                }
+                catch (XunitException)
+                {
+                    logger.LogWarning(response.ToString());
+                    logger.LogWarning(responseText);
+                    throw;
+                }
+
+                string output = string.Empty;
+                deployer.HostProcess.OutputDataReceived += (sender, args) => output += args.Data + '\n';
+
+                SendSIGINT(deployer.HostProcess.Id);
+
+                deployer.HostProcess.WaitForExit();
+                output = output.Trim('\n');
+
+                Assert.Equal(output, "Application is shutting down...\n" +
+                                     "Stopping firing\n" +
+                                     "Stopping end\n" +
+                                     "Stopped firing\n" +
+                                     "Stopped end");
+            }
+        }
+
+
+        private static void SendSIGINT(int processId)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "kill",
+                Arguments = processId.ToString(),
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var process = Process.Start(startInfo);
+            process.WaitForExit(1000);
+            if (!process.HasExited)
+            {
+                process.Kill();
+            }
+
+            Assert.Equal(0, process.ExitCode);
+        }
+    }
+}
