2065ec012d95b0c7f31c4efbe034daa0d0726a63
Vance Morrison <vancemorrison@comcast.net>
2017-02-14 13:10:37 -0800

    Changed level to critical

--- src/Microsoft.AspNetCore.Hosting/Internal/HostingLoggerExtensions.cs
+++ src/Microsoft.AspNetCore.Hosting/Internal/HostingLoggerExtensions.cs
@@ -18,8 +18,8 @@ namespace Microsoft.AspNetCore.Hosting.Internal
 
         public static IDisposable RequestScope(this ILogger logger, HttpContext httpContext)
         {
-            // to avoid allocation, return a null scope if the logger is not on.  
-            if (logger.IsEnabled(LogLevel.Information))
+            // to avoid allocation, return a null scope if the logger is not on at least to some degree.   
+            if (logger.IsEnabled(LogLevel.Critical))
                 return logger.BeginScope(new HostingLogScope(httpContext));
             else
                 return null;
