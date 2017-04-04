3a33d108c3c322fdabcc4a87119c2b053c6afbd5
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-04-04 12:19:29 -0700

    add global log

--- src/Microsoft.AspNetCore.Server.IntegrationTesting/xunit/TestLogging.cs
+++ src/Microsoft.AspNetCore.Server.IntegrationTesting/xunit/TestLogging.cs
@@ -10,6 +10,9 @@ namespace Microsoft.AspNetCore.Server.IntegrationTesting.xunit
 {
     public static class TestLogging
     {
+        // Need to qualify because of Serilog.ILogger :(
+        private static readonly Extensions.Logging.ILogger GlobalLogger = CreateGlobalLogger();
+
         public static readonly string OutputDirectoryEnvironmentVariableName = "ASPNETCORE_TEST_LOG_DIR";
         public static readonly string TestOutputRoot = Environment.GetEnvironmentVariable(OutputDirectoryEnvironmentVariableName);
 
@@ -17,8 +20,14 @@ namespace Microsoft.AspNetCore.Server.IntegrationTesting.xunit
         {
             loggerFactory = CreateLoggerFactory<TTestClass>(output, testName);
             var logger = loggerFactory.CreateLogger("TestLifetime");
+
+            GlobalLogger.LogInformation("Starting test {testName}", testName);
             logger.LogInformation("Starting test {testName}", testName);
-            return new Disposable(() => logger.LogInformation("Finished test {testName}", testName));
+            return new Disposable(() =>
+            {
+                GlobalLogger.LogInformation("Finished test {testName}", testName);
+                logger.LogInformation("Finished test {testName}", testName);
+            });
         }
 
         public static ILoggerFactory CreateLoggerFactory<TTestClass>(ITestOutputHelper output, [CallerMemberName] string testName = null)
@@ -26,40 +35,73 @@ namespace Microsoft.AspNetCore.Server.IntegrationTesting.xunit
             var loggerFactory = new LoggerFactory();
             loggerFactory.AddXunit(output, LogLevel.Debug);
 
+            var testClass = typeof(TTestClass).GetTypeInfo();
+            var asmName = testClass.Assembly.GetName().Name;
+            var className = testClass.FullName;
+
+            // Try to shorten the class name using the assembly name
+            if (className.StartsWith(asmName + "."))
+            {
+                className = className.Substring(asmName.Length + 1);
+            }
+
+            var testOutputFile = Path.Combine(asmName, className, $"{testName}.log");
+            AddFileLogging(loggerFactory, testOutputFile);
+
+            return loggerFactory;
+        }
+
+        // Need to qualify because of Serilog.ILogger :(
+        private static Extensions.Logging.ILogger CreateGlobalLogger()
+        {
+            var loggerFactory = new LoggerFactory();
+
+#if NET46
+            var appName = Assembly.GetEntryAssembly().GetName().Name;
+#else
+            // GROOOSSS but should work...
+            string appName;
+            var files = Directory.GetFiles(AppContext.BaseDirectory, "*.runtimeconfig.json");
+            if(files.Length == 0)
+            {
+                // Even Grosser
+                appName = Guid.NewGuid().ToString("N");
+            }
+            else
+            {
+                appName = Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(files[0]));
+            }
+#endif
+
+            AddFileLogging(loggerFactory, Path.Combine(appName, "global.log"));
+
+            return loggerFactory.CreateLogger("GlobalTestLog");
+        }
+
+        private static void AddFileLogging(ILoggerFactory loggerFactory, string fileName)
+        {
             if (!string.IsNullOrEmpty(TestOutputRoot))
             {
-                var testClass = typeof(TTestClass).GetTypeInfo();
-                var asmName = testClass.Assembly.GetName().Name;
-                var className = testClass.FullName;
+                fileName = Path.Combine(TestOutputRoot, fileName);
 
-                // Try to shorten the class name using the assembly name
-                if (className.StartsWith(asmName + "."))
+                var dir = Path.GetDirectoryName(fileName);
+                if (!Directory.Exists(dir))
                 {
-                    className = className.Substring(asmName.Length + 1);
+                    Directory.CreateDirectory(dir);
                 }
 
-                var testOutputDir = Path.Combine(TestOutputRoot, asmName, className);
-                if (!Directory.Exists(testOutputDir))
+                if (File.Exists(fileName))
                 {
-                    Directory.CreateDirectory(testOutputDir);
-                }
-
-                var testOutputFile = Path.Combine(testOutputDir, $"{testName}.log");
-
-                if (File.Exists(testOutputFile))
-                {
-                    File.Delete(testOutputFile);
+                    File.Delete(fileName);
                 }
 
                 var serilogger = new LoggerConfiguration()
                     .Enrich.FromLogContext()
                     .MinimumLevel.Verbose()
-                    .WriteTo.File(testOutputFile, flushToDiskInterval: TimeSpan.FromSeconds(1))
+                    .WriteTo.File(fileName, flushToDiskInterval: TimeSpan.FromSeconds(1))
                     .CreateLogger();
                 loggerFactory.AddSerilog(serilogger);
             }
-
-            return loggerFactory;
         }
 
         private class Disposable : IDisposable
