b90e5c349e4b141ac2f65980c58c0054c6a8c9f3
Marcin Polewski <marcin.polewski@gmail.com>
2018-04-15 22:49:48 -0500

    Adding MSMQ host sample

---
+++ samples/SampleMsmqHost/ConsoleInputService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleMsmqHost
+{
+    public class ConsoleInputService : IHostedService, IDisposable
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public IMsmqConnection Connection { get; }
+
+        public ConsoleInputService(IMsmqConnection connection)
+        {
+            Connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        public void Dispose()
+        {
+            _cancellationTokenSource.Dispose();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var stoppingToken = _cancellationTokenSource.Token;
+
+            // run the read loop in a background thread
+            Task.Run(() => ReadLoopAsync(stoppingToken), cancellationToken);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            // tell the read loop to stop
+            _cancellationTokenSource.Cancel();
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ReadLoopAsync(CancellationToken cancellationToken)
+        {
+            await Console.Out.WriteLineAsync("Enter your text message and press ENTER...");
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // read a text message from the user
+                cancellationToken.ThrowIfCancellationRequested();
+                var text = await Console.In.ReadLineAsync();
+
+                // send the text message to the queue
+                cancellationToken.ThrowIfCancellationRequested();
+                if (!string.IsNullOrEmpty(text))
+                    await Connection.SendTextAsync(text, cancellationToken);
+            }
+        }
+
+    }
+}
---
+++ samples/SampleMsmqHost/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Messaging;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleMsmqHost
+{
+    public static class Program
+    {
+        // Before running this program, please make sure to install MSMQ
+        // and create the ".\private$\SampleQueue" queue on your local machine.
+
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddCommandLine(args);
+                })
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddOptions();
+
+                    services.Configure<MsmqOptions>(options =>
+                    {
+                        options.Path = @".\private$\SampleQueue";
+                        options.AccessMode = QueueAccessMode.SendAndReceive;
+                    });
+
+                    services.AddSingleton<IMsmqConnection, MsmqConnection>();
+                    services.AddTransient<IMsmqProcessor, MsmqProcessor>();
+                    services.AddTransient<IHostedService, MsmqService>();
+                    services.AddTransient<IHostedService, ConsoleInputService>();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
+        }
+
+    }
+}
